a446137d7e2b39a181dc55e90bbc4ac94c84f4e7
zacyang <yangmeansyoung@gmail.com>
2015-02-18 10:19:40 +0800

    [GURU] Remove unused tests

--- test/guru/core_test.clj
+++ test/guru/core_test.clj
@@ -2,12 +2,6 @@
   (:require [clojure.test :refer :all]
             [guru.core :refer :all :as functionsUnderTest]))
 
-(deftest route-function-tests
-  (def expected-json {:key "123"})
-  (testing "should give a json in 200 OK"
-    (is (= expected-json (functionsUnderTest/handle request))))
-  (testing "should give 404 when no associated item is found")
-)
 (deftest a-test
   (testing "FIXME, I fail."
     (is (= 0 0))))
